e7b9700cc87d50f8525595e6ab2769a24b604ce1
Arvid Berndtsson <arvid@berndtsson.me>
2024-06-19 20:39:48 +0200

    fix: modify get_active_computer import

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,8 @@
 # Create API routes and include them in the main application
 from fastapi import APIRouter
-from .endpoints.computer_information import get_active_computers
+from .endpoints.computer_information import get_active_computer
 
 api_router = APIRouter()
 
-api_router.include_router(get_active_computers.router, tags=[
+api_router.include_router(get_active_computer.router, tags=[
                           "Computer Information"], prefix="/info")
